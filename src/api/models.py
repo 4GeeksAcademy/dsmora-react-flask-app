271ec3f03e4a22f880c93333a8d4ce95fad4244a
David S. Mora Toro <39775861+dsmora@users.noreply.github.com>
2025-06-23 17:06:44 +0000

    add models

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
@@ -11,10 +12,43 @@ class User(db.Model):
         String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    favorites: Mapped[List['Favorite']] = relationship(
+        back_populates="user")
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active,
+            "favorites": list(map(lambda x: x.serialize(), self.favorites))
+            # do not serialize the password, its a security breach
+        }
+
+
+class Favorite(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    product_id: Mapped[int] = mapped_column(ForeignKey("product.id"))
+    product: Mapped['Product'] = relationship(back_populates="favorite")
+    user: Mapped['User'] = relationship(back_populates="favorites")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "product": self.product.serialize(),
+        }
+
+
+class Product(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(
+        String(120), nullable=False)
+    favorite: Mapped[List['Favorite']] = relationship(back_populates="product")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
             # do not serialize the password, its a security breach
         }
